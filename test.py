d41bdc9e26e53f5e1ea68a3ff4f35c35f8610cf7
prazwalstark <prazwolstark@gmail.com>
2024-03-22 10:40:17 +0545

    3-22

---
+++ test.py
@@ -0,0 +1,37 @@
+# import sys
+# class Example:
+#     def __init__(self):
+#         pass
+
+#     def helloWorld():
+#         print("hello world")
+#         pass
+
+# eg = Example
+# eg.helloWorld()
+
+# num1 = 123.123
+# num2 = int(num1)
+# print(num2)
+# print("The number {} has it's equivalent integer value {}".format(num1,num2))
+# def functionDoc():
+#     """This function does nothing."""
+#     print("Hello World")
+#     return None
+# print(functionDoc.__doc__)
+# help(functionDoc)
+#  def greetingUser(name,greeting="Hello!"):
+    # """ This function accepts two parameters. The first one is name and second is greeting and hence welcomes the user."""
+    # return ("{} {}".format(greeting,name))
+# 
+# print
+# help=input("Do you want help regarding the function?(y/n)")
+# if(help == 'y'):
+    # print(greetingUser.__doc__)
+# name = input("Enter the name: ")
+# print(greetingUser("Mr. "+name+". Welcome Back"))
+# from dateutil.parser import parse
+# dt = parse("Today the date is 21st of March, 2024 and the time is 9:15 pm",fuzzy=True)
+# print(dt)
+print(~105)
+
